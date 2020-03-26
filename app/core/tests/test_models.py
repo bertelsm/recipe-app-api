4dbd15e0b3fb71ed2eabdfd27cbb692ee99ddf5b
Ubuntu <ubuntu@ip-172-31-23-168.us-west-2.compute.internal>
2020-03-26 20:21:52 +0000

    Added multiple user and admin model tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -17,26 +17,26 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
 
-def test_new_user_email_normalized(self):
-    """Test the email for a new user is normalized"""
-    email = 'test@LONONAPPDEV.COM'
-    user = get_user_model().objects.create_user(email, 'test123')
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@LONONAPPDEV.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
 
-    self.assertEqual(user.email, email.lower())
-
-
-def test_new_user_invalid_email(self):
-    """Test creating user with no email raises error"""
-    with self.assertRaises(ValueError):
-        get_user_model().objects.create_user(None, 'test123')
+        self.assertEqual(user.email, email.lower())
 
 
-def test_create_new_supersuser(self):
-    """Test creating a new superuser"""
-    user = get_user_model().objects.create_superuser(
-        'test@londonappdev.com'
-        'test123'
-    )
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
 
-    self.assertTrue(user.is_superuser)
-    self.assertTrue(user.is_staff)
+
+    def test_new_supersuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@londonappdev.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
